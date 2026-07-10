c4edf384800bd09769d5dc2dcc950a871d559b44
AFD 25-26. Des. Aplicacións Tec.Web <datw26@alupostal4.local>
2026-07-10 13:35:03 +0200

    10/077

---
+++ config.py
@@ -0,0 +1,8 @@
+APP_BASE_URL="http://localhost:5000"
+SECRET_KEY="iausdgygss45gr8ssad546354fa59asd56as4d8as12"
+DB_HOST="localhost"
+DB_PORT=5432
+DB_USER="postgres"
+DB_PASSWORD="abc123"
+DB_NAME="acortador_url"
+
